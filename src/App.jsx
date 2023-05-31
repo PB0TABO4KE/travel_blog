c508680a4964a2019b558bf45b1185fd568e9692
PB0TABO4KE <qseo.lukovnikov@gmail.com>
2023-05-31 14:34:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Команда #1 самая лучшая!!11</h1>
+    </div>
+  );
+}
+
+export default App;
